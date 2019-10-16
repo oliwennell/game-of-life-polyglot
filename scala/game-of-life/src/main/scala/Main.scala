69488ee7bb2b22d29837510bb49ed3a9e16b09fc
Oli Wennell <oli.wennell@gmail.com>
2019-10-16 15:06:06 +0100

    :construction: Printing out a hard-coded grid

--- scala/game-of-life/src/main/scala/Main.scala
+++ scala/game-of-life/src/main/scala/Main.scala
@@ -1,3 +1,16 @@
+
 object Main extends App {
-  println("Hello, World!")
+
+  val cells = List[List[String]](
+    List("X", " ", " ", " "),
+    List(" ", "X", " ", " "),
+    List(" ", " ", "X", " "),
+    List(" ", " ", " ", "X"),
+  )
+
+  def rowToString(row: List[String]) = row.reduce((a, b) => a + b)
+
+  val rowStrings = cells.map(rowToString)
+  var all = rowStrings.reduce((a,b) => a + "\n" + b)
+  println(all)
 }
