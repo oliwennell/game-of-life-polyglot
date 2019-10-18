020b7f5969ac60e8c1db91dc9c46e4ebec3778ed
Oli Wennell <oli.wennell@gmail.com>
2019-10-18 14:01:36 +0100

    :fire: Removing temp build things

--- scala/game-of-life/src/main/scala/Main.scala
+++ scala/game-of-life/src/main/scala/Main.scala
@@ -1,16 +1,30 @@
 
 object Main extends App {
+    
+  // val cells = List[List[String]](
+  //   List("X", " ", " ", " "),
+  //   List(" ", "X", " ", " "),
+  //   List(" ", " ", "X", " "),
+  //   List(" ", " ", " ", "X"),
+  // )
 
-  val cells = List[List[String]](
-    List("X", " ", " ", " "),
-    List(" ", "X", " ", " "),
-    List(" ", " ", "X", " "),
-    List(" ", " ", " ", "X"),
-  )
+  // def rowToString(row: List[String]) = row.reduce((a, b) => a + b)
 
-  def rowToString(row: List[String]) = row.reduce((a, b) => a + b)
+  // def simulate(cells) = cells
 
-  val rowStrings = cells.map(rowToString)
-  var all = rowStrings.reduce((a,b) => a + "\n" + b)
-  println(all)
+  // def printGrid(cells) {
+  //   val rowStrings = cells.map(rowToString)
+  //   var all = rowStrings.reduce((a,b) => a + "\n" + b)
+  //   println(all)
+  // }
+
+  // def main() {
+  //   var cells;
+  //   for (i <- 0 to 5) {
+  //     cells = simulate(cells)
+  //     printGrid(cells)
+  //   }
+  // }
+  
+  println("yo")
 }
