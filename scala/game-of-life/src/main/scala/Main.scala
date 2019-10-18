a86c845d11775fd5d7322d4dab1e9e0dc34b8902
Oli Wennell <oli.wennell@gmail.com>
2019-10-18 14:43:02 +0100

    :construction: Putting scaffolding in place for actually simulating game-of-life

--- scala/game-of-life/src/main/scala/Main.scala
+++ scala/game-of-life/src/main/scala/Main.scala
@@ -1,30 +1,54 @@
 
 object Main extends App {
-    
-  // val cells = List[List[String]](
-  //   List("X", " ", " ", " "),
-  //   List(" ", "X", " ", " "),
-  //   List(" ", " ", "X", " "),
-  //   List(" ", " ", " ", "X"),
-  // )
 
-  // def rowToString(row: List[String]) = row.reduce((a, b) => a + b)
-
-  // def simulate(cells) = cells
-
-  // def printGrid(cells) {
-  //   val rowStrings = cells.map(rowToString)
-  //   var all = rowStrings.reduce((a,b) => a + "\n" + b)
-  //   println(all)
-  // }
-
-  // def main() {
-  //   var cells;
-  //   for (i <- 0 to 5) {
-  //     cells = simulate(cells)
-  //     printGrid(cells)
-  //   }
-  // }
+  type Grid = List[List[Cell]]
   
-  println("yo")
+  abstract class Cell
+  case class AliveCell() extends Cell
+  case class DeadCell() extends Cell
+
+  val initialCells = List[List[Cell]](
+    List(new AliveCell, new DeadCell,  new DeadCell,  new DeadCell),
+    List(new DeadCell,  new AliveCell, new DeadCell,  new DeadCell),
+    List(new DeadCell,  new DeadCell,  new AliveCell, new DeadCell),
+    List(new DeadCell,  new DeadCell,  new DeadCell,  new AliveCell),
+  )
+
+  def shouldLiveOn(cell: Cell, grid: Grid) =
+    cell match {
+      case AliveCell() => true
+      case DeadCell() => false
+    }
+
+  def simulateRow(row: List[Cell], grid: Grid) =
+    row.map(cell => shouldLiveOn(cell, grid))
+       .map(s => s match {
+         case true => new AliveCell
+         case false => new DeadCell
+      })
+
+  def simulate(grid: Grid) = 
+    grid.map(row => simulateRow(row, grid))
+
+  def cellToString(cell: Cell) = 
+    cell match {
+      case DeadCell() => " "
+      case AliveCell() => "X"
+    }
+
+  def rowToString(row: List[Cell]) =
+    row.map(cellToString)
+       .reduce((a, b) => a + b)
+
+  def printGrid(cells: Grid) {
+    val rowStrings = cells.map(rowToString)
+    var all = rowStrings.reduce((a,b) => a + "\n" + b)
+    println(all)
+  }
+
+  var cells = initialCells;
+  for (i <- 0 to 5) {
+    cells = simulate(cells)
+    printGrid(cells)
+  }
 }
